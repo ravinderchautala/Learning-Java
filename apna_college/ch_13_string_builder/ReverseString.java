2eb0710eb9eff5e600f93ea9dc5232e4d91f6e3e
ravinderchautala <ryvinder@gmail.com>
2022-06-22 19:28:21 +0530

    string builder tutorial completed

---
+++ apna_college/ch_13_string_builder/ReverseString.java
@@ -0,0 +1,21 @@
+package apna_college.ch_13_string_builder;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        StringBuilder str = new StringBuilder(sc.nextLine());
+        for (int i = 0; i < str.length()/2; i++) {
+            int front = i;
+            int back = str.length() - 1 - i;
+            char frontChar = str.charAt(front);
+            char backChar = str.charAt(back);
+            str.setCharAt(front,backChar);
+            str.setCharAt(back, frontChar);
+
+        }
+        System.out.println(str);
+    }
+}
---
+++ apna_college/ch_13_string_builder/StringsBuilder.java
@@ -0,0 +1,30 @@
+package apna_college.ch_13_string_builder;
+public class StringsBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Ravinder");
+        System.out.println(sb);
+
+        // char at index 0
+        // System.out.println(sb.charAt(1));
+
+        // set char at index 0
+        // sb.setCharAt(0, 'v');
+        // System.out.println(sb);
+        
+        // inserting string
+        sb.insert(2,'V');
+        System.out.println(sb);
+
+        // deleting string
+        sb.delete(2,4);
+        System.out.println(sb);
+
+        // appending value
+        StringBuilder grt = new StringBuilder("h");
+        grt.append("e");
+        grt.append("l");
+        grt.append("l");
+        grt.append("o");
+        System.out.println(grt);
+    }
+}
