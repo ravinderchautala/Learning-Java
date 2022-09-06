2f7820ac6cbe54bb8b69f005bd39b4352b4ad220
ravinderchautala <ryvinder@gmail.com>
2022-09-07 04:04:42 +0530

    bug fixed

--- apna_college/ch02_second_code/Addition.java
+++ apna_college/ch02_second_code/Addition.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Addition {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number:");
-        int a = sc.nextInt();
-        System.out.print("Enter second number:");
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println( "The addition of two numbers is: " + sum );
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter first number:");
+            int a = sc.nextInt();
+            System.out.print("Enter second number:");
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println( "The addition of two numbers is: " + sum );
+        }
     }
 }
