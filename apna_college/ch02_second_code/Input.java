d356600add04a5c515b5c137844e27500350f687
ravinderchautala <ryvinder@gmail.com>
2022-09-07 04:00:52 +0530

    bug fixed

--- apna_college/ch02_second_code/Input.java
+++ apna_college/ch02_second_code/Input.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name:");
-        String name = sc.nextLine();
-        System.out.println(name);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter your name:");
+            String name = sc.nextLine();
+            System.out.println(name);
+        }
     }
 }
