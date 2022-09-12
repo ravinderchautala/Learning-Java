7ac34552cb6971aae9e5f8bc63db3aa27d26e54e
ravinderchautala <ryvinder@gmail.com>
2022-09-13 03:03:27 +0530

    bug fixed

--- apna_college/ch06_advance_patterns/Butterfly.java
+++ apna_college/ch06_advance_patterns/Butterfly.java
@@ -4,30 +4,40 @@ import java.util.Scanner;
 
 public class Butterfly {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enetr a value: ");
-        int n = sc.nextInt();
-        for(int i =1;i<=n;i++){
-            for(int j = 1; j<=i;j++){
-                System.out.print("*");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enetr a value: ");
+            int n = sc.nextInt();
+            // upper half
+            for(int i =1;i<=n;i++){
+                // left-side *
+                for(int j = 1; j<=i;j++){
+                    System.out.print("*");
+                }
+                // spaces
+                for(int j = 1;j<=2*(n-i);j++){
+                    System.out.print(" ");
+                }
+                // right-side *
+                for(int j = 1; j<=i;j++){
+                    System.out.print("*");
+                }System.out.println();
             }
-            for(int j = 1;j<=2*(n-i);j++){
-                System.out.print(" ");
+
+            // lower half
+            for(int i =n;i>=1;i--){
+                // left-side *
+                for(int j = 1; j<=i;j++){
+                    System.out.print("*");
+                }
+                // spaces
+                for(int j = 1;j<=2*(n-i);j++){
+                    System.out.print(" ");
+                }
+                // right-side *
+                for(int j = 1; j<=i;j++){
+                    System.out.print("*");
+                }System.out.println();
             }
-            for(int j = 1; j<=i;j++){
-                System.out.print("*");
-            }System.out.println();
-        }
-        for(int i =n;i>=1;i--){
-            for(int j = 1; j<=i;j++){
-                System.out.print("*");
-            }
-            for(int j = 1;j<=2*(n-i);j++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j<=i;j++){
-                System.out.print("*");
-            }System.out.println();
         }
     }
 }
