a796afb50e5db96c0e596d156966ce39c74f7b2a
ravinderchautala <ryvinder@gmail.com>
2022-09-12 21:03:07 +0530

    prime number problem solved

--- apna_college/ch04_loops/homework/Prime.java
+++ apna_college/ch04_loops/homework/Prime.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int a = sc.nextInt();
-        if(a>5){
-        if(a%2==0 || a%3==0 || a%5==0){
-            System.out.println("Not a Prime");
-        }else{
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int a = sc.nextInt();
+            if(a>5){
+            if(a%2==0 || a%3==0 || a%5==0){
+                System.out.println("Not a Prime");
+            }else{
+                System.out.println("Number is Prime");
+            }
+   }else if(a<=5 && a>1){
             System.out.println("Number is Prime");
+   }else{
+            System.out.println("Enter a valid number");
+   }
         }
-    }else if(a<=5 && a>1){
-        System.out.println("Number is Prime");
-    }else{
-        System.out.println("Enter a valid number");
-    }
     }
 }
