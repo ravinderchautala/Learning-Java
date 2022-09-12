359ba3c43daa55e17c1a842e313e54508a92172c
ravinderchautala <ryvinder@gmail.com>
2022-09-13 04:14:16 +0530

    palindrom pattern problem solved

--- apna_college/ch05_patterns/homework/NumberPyramid.java
+++ apna_college/ch05_patterns/homework/NumberPyramid.java
@@ -7,15 +7,13 @@ public class NumberPyramid {
             System.out.print("Enter a number: ");
             int n = sc.nextInt();
             for(int i = 1; i<=n;i++){
-                for(int j = n-i;j>0;j--){
-                    System.out.print("  ");
-                }for(int j = 0;j<i;j++){
-                    System.out.print(" "+ (i-j));
-                }if(i>=2){
-                    for(int j = 2;j<=i;j++){
-                        System.out.print(" "+j);
+                for(int j =1;j<=n-i;j++){
+                    System.out.print(" ");
+                }for(int j = i;j>0;j--){
+                    System.out.print(j);
+                }for(int j = 2;j<=i;j++){
+                        System.out.print(j);
                     }
-                }
                 System.out.println();
             }
         }
--- apna_college/ch06_advance_patterns/PalindromicPattern.java
+++ apna_college/ch06_advance_patterns/PalindromicPattern.java
@@ -8,12 +8,16 @@ public class PalindromicPattern {
             System.out.print("Enter a number: ");
             int n = sc.nextInt();
             for(int i=1;i<=n;i++){
+                // spaces
                 for(int j=1;j<=n-i;j++){
                     System.out.print(" ");
                 }
+                // first half
                 for(int j=i;j>=1;j--){
                     System.out.print(j);
-                }for(int j=2;j<=i;j++){
+                }
+                // second half
+                for(int j=2;j<=i;j++){
                     System.out.print(j);
                 }
                 System.out.println("");
