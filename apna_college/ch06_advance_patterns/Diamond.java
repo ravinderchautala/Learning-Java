83614632141c4f66cb5e940be4121417c91b34ed
ravinderchautala <ryvinder@gmail.com>
2022-09-13 04:26:26 +0530

    diamond pattern problem solved

--- apna_college/ch06_advance_patterns/Diamond.java
+++ apna_college/ch06_advance_patterns/Diamond.java
@@ -7,21 +7,37 @@ public class Diamond {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }for(int j=1;j<=2*i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+        // upper half
+       for(int i =1;i<=n;i++){
+        // spaces
+        for(int j = 1; j<=n-i;j++){
+            System.out.print(" ");
         }
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }for(int j=1;j<=2*i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+        // first half
+        for(int j = 1;j<=i;j++){
+            System.out.print("*");
+        }
+        // second half
+        for(int j = 2;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+        // lower half
+       for(int i =n;i>=1;i--){
+        // spaces
+        for(int j = 1; j<=n-i;j++){
+            System.out.print(" ");
+        }
+        // first half
+        for(int j = 1;j<=i;j++){
+            System.out.print("*");
+        }
+        // second half
+        for(int j = 2;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
         }
     }
 }
