b42003b431aa6b1445bedb2bd0f1dff507847f25
ravinderchautala <ryvinder@gmail.com>
2022-09-13 01:48:39 +0530

    number triangle pattern problrm solved

--- apna_college/ch05_patterns/homework/SolidRhombus.java
+++ apna_college/ch05_patterns/homework/SolidRhombus.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class SolidRhombus {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the row no. ");
-        int n = sc.nextInt();
-        System.out.print("Enter the column no. ");
-        int m = sc.nextInt();
-        for(int i = 0; i<n;i++){
-            for(int j = m-i; j>0;j--){
-                System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the row no. ");
+            int n = sc.nextInt();
+            System.out.print("Enter the column no. ");
+            int m = sc.nextInt();
+            for(int i = 0; i<n;i++){
+                for(int j = m-i; j>0;j--){
+                    System.out.print(" ");
+                }
+                for(int j = 0;j<m;j++){
+                    System.out.print("*");
+                }System.out.println();
             }
-            for(int j = 0;j<m;j++){
-                System.out.print("*");
-            }System.out.println();
         }
     }
 }
