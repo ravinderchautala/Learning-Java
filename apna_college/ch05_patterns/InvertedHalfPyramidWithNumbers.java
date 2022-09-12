2bb0234a5e734a1e2abeac6ec09d59e3697883c7
ravinderchautala <ryvinder@gmail.com>
2022-09-13 00:36:29 +0530

    inverted half pyramid with numbers pattern problem solved

--- apna_college/ch05_patterns/InvertedHalfPyramidWithNumbers.java
+++ apna_college/ch05_patterns/InvertedHalfPyramidWithNumbers.java
@@ -2,13 +2,14 @@ package apna_college.ch05_patterns;
 import java.util.*;
 public class InvertedHalfPyramidWithNumbers {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter a value:");
-        int n = sc.nextInt();
-        for(int i=n; i>=0; i--){
-            for(int j=1; j<=i;j++){
-            System.out.print(j+" ");
-        }System.out.println("");
-    }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a value:");
+            int n = sc.nextInt();
+            for(int i =n ;i>=1;i--){
+                for(int j =1;j<=i;j++){
+                    System.out.print(j+" ");
+                }System.out.println();
+                }
+        }
     }
 }
