e23165364d681db842797b6864c092ae71362fac
ravinderchautala <ryvinder@gmail.com>
2022-09-13 00:05:59 +0530

    half pyramid pattern problem solved

--- apna_college/ch05_patterns/HalfPyramid.java
+++ apna_college/ch05_patterns/HalfPyramid.java
@@ -7,10 +7,10 @@ public class HalfPyramid {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter a value:");
         int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i;j++){
-            System.out.print("*");
-        }System.out.println("");
-    }
+        for(int i = 1; i<=n;i++){
+            for(int j = 0 ;j<i;j++){
+                System.out.print("*");
+            }System.out.println("");
+        }
 }
 }
--- apna_college/ch05_patterns/HollowRectangle.java
+++ apna_college/ch05_patterns/HollowRectangle.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 
 public class HollowRectangle {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the value of rows:");
-        int n=sc.nextInt();
-        System.out.print("Enter the value of columns:");
-        int m=sc.nextInt();
-        for(int i =1;i<=n;i++){
-                for(int j =1; j<=m;j++){
-                    if(j==m ||j==1||i==1||i==n){
-                        System.out.print("*");
-                    }else{
-                        System.out.print(" ");
-                    }
-                }System.out.println("");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the value of rows:");
+            int n=sc.nextInt();
+            System.out.print("Enter the value of columns:");
+            int m=sc.nextInt();
+            for(int i =1;i<=n;i++){
+                    for(int j =1; j<=m;j++){
+                        if(j==m ||j==1||i==1||i==n){
+                            System.out.print("*");
+                        }else{
+                            System.out.print(" ");
+                        }
+                    }System.out.println("");
+            }
         }
     }
 }
