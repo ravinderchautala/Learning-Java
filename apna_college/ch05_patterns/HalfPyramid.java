9a4d3f145b3f1c7171b15d5b3db13698a53d4fd5
ravinderchautala <ryvinder@gmail.com>
2022-09-13 00:09:54 +0530

    inverted half pyramid pattern problem solved

--- apna_college/ch05_patterns/HalfPyramid.java
+++ apna_college/ch05_patterns/HalfPyramid.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class HalfPyramid {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter a value:");
-        int n = sc.nextInt();
-        for(int i = 1; i<=n;i++){
-            for(int j = 0 ;j<i;j++){
-                System.out.print("*");
-            }System.out.println("");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a value:");
+            int n = sc.nextInt();
+            for(int i = 1; i<=n;i++){
+                for(int j = 0 ;j<i;j++){
+                    System.out.print("*");
+                }System.out.println("");
+            }
         }
 }
 }
--- apna_college/ch05_patterns/HalfPyramidInverted.java
+++ apna_college/ch05_patterns/HalfPyramidInverted.java
@@ -2,16 +2,15 @@ package apna_college.ch05_patterns;
 import java.util.*;
 public class HalfPyramidInverted {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter a value:");
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i;j++){
-            System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a value:");
+            int n = sc.nextInt();
+            for(int i= n;i>=1;i--){
+                for(int j = 1 ;j<=i;j++){
+                    System.out.print("*");
+                }System.out.println("");
+            }
         }
-            for(int j=1;j<=i;j++){
-            System.out.print("*");
-        }System.out.println("");
-    }
+      
     }
 }
