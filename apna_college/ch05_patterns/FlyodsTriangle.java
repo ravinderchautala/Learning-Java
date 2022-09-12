6b53c67f1c01b4cde03b1fb26e392bdd633ff1cc
ravinderchautala <ryvinder@gmail.com>
2022-09-13 01:00:03 +0530

    floyd triangle pattern problem solved

--- apna_college/ch05_patterns/FlyodsTriangle.java
+++ apna_college/ch05_patterns/FlyodsTriangle.java
@@ -3,14 +3,16 @@ import java.util.*;
 
 public class FlyodsTriangle {
     public static void main(String[] args) {
-    Scanner sc =new Scanner(System.in);
+    try (Scanner sc = new Scanner(System.in)) {
         System.out.print("Enter a value:");
         int n = sc.nextInt();
-        int num=1;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i;j++){
-            System.out.print(num++ + " ");
-        }System.out.println("");
+        int num =1;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }System.out.println();
+        }
     }
 }
 }
