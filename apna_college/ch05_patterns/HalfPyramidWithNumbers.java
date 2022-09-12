cec655f532a3cbfbc70beb00644f585b75fffb5d
ravinderchautala <ryvinder@gmail.com>
2022-09-13 00:30:12 +0530

    half pyramid with numbers pattern problem solved

--- apna_college/ch05_patterns/HalfPyramidWithNumbers.java
+++ apna_college/ch05_patterns/HalfPyramidWithNumbers.java
@@ -2,13 +2,15 @@ package apna_college.ch05_patterns;
 import java.util.*;
 public class HalfPyramidWithNumbers {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter a value:");
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i;j++){
-            System.out.print(j+ " ");
-        }System.out.println("");
-    }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a value:");
+            int n = sc.nextInt();
+            for(int i =1;i<=n;i++){
+                for(int j =1; j<=i;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
--- apna_college/ch05_patterns/InvertedHalfPyramid.java
+++ apna_college/ch05_patterns/InvertedHalfPyramid.java
@@ -1,19 +1,21 @@
 package apna_college.ch05_patterns;
+
 import java.util.*;
+
 public class InvertedHalfPyramid {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter a value:");
             int n = sc.nextInt();
-           for(int i =1;i<=n;i++){
-            for(int j= n-i;j>=1;j--){
-                System.out.print(" ");
+            for (int i = 1; i <= n; i++) {
+                for (int j = n - i; j >= 1; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= i; k++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
             }
-            for(int k =1; k<=i;k++){
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
         }
     }
 }
