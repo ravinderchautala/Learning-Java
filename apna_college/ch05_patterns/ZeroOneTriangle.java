7796fbf62197500c8d774150a93717ed6eaab8fb
ravinderchautala <ryvinder@gmail.com>
2022-09-13 01:26:42 +0530

    zero-one triangle pattern problem solved

--- apna_college/ch05_patterns/ZeroOneTriangle.java
+++ apna_college/ch05_patterns/ZeroOneTriangle.java
@@ -2,17 +2,18 @@ package apna_college.ch05_patterns;
 import java.util.*;
 public class ZeroOneTriangle {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter a value:");
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i;j++){
-            if((i+j)%2==0){
-                System.out.print("1 ");
-            }else{
-                System.out.print("0 ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a value:");
+            int n = sc.nextInt();
+            for(int i = 1;i<=n;i++){
+                for(int j = 1 ; j<=i;j++){
+                    if((i+j)%2==0){
+                        System.out.print(1+" ");
+                    }else{
+                        System.out.print(0+" ");
+                    }
+                }System.out.println();
             }
-        }System.out.println("");
-    }
+        }
     }
 }
