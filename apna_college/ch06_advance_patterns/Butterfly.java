d5393c5615e0b9e9e08a670180e45747d8bfec03
ravinderchautala <ryvinder@gmail.com>
2022-09-13 02:55:22 +0530

    butterfly pattern problem solved

--- apna_college/ch06_advance_patterns/Butterfly.java
+++ apna_college/ch06_advance_patterns/Butterfly.java
@@ -7,24 +7,27 @@ public class Butterfly {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enetr a value: ");
         int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i;j++){
-            System.out.print("*");
+        for(int i =1;i<=n;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
         }
-        for(int j=1;j<=2*(n-i);j++){
-            System.out.print(" ");
-        }for(int j=1; j<=i;j++){
-            System.out.print("*");
-        }System.out.println("");
-    }for(int i=n; i>=1; i--){
-        for(int j=1; j<=i;j++){
-        System.out.print("*");
-    }
-    for(int j=1;j<=2*(n-i);j++){
-        System.out.print(" ");
-    }for(int j=1; j<=i;j++){
-        System.out.print("*");
-    }System.out.println("");
-}
+        for(int i =n;i>=1;i--){
+            for(int j = 1; j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
     }
 }
