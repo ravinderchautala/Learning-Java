9a1c66c1367dde46be29f98dacfb9ec349f529b0
ravinderchautala <ryvinder@gmail.com>
2022-09-18 14:06:12 +0530

    bit conversion problem solved

---
+++ apna_college/ch15_bit_manipulation/homework/Conversion.java
@@ -0,0 +1,29 @@
+package apna_college.ch15_bit_manipulation.homework;
+
+import java.util.Scanner;
+
+public class Conversion {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number to convert: ");
+                int n = sc.nextInt();
+                String str ="";
+                System.out.println("Type one of (binary/bit): ");
+                String s = sc.next();
+                    while(n>0){
+                        str  = ((n%2==0) ? "0" : "1")+str;
+                        n= n/2;
+                    }
+                    System.out.println(str);
+                    int d = 0;
+                    for(int i=str.length()-1;i>=0;i--){
+                        if(str.charAt(i)=='0'){
+                            d+=0;
+                        }else if(str.charAt(i)=='1'){
+                            d+=Math.pow(2,i);
+                        }
+                    }
+                    System.out.println(d);
+        }
+    }
+}
--- apna_college/ch15_bit_manipulation/homework/Pow.java
+++ apna_college/ch15_bit_manipulation/homework/Pow.java
@@ -4,20 +4,26 @@ import java.util.Scanner;
 
 public class Pow {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number to check: ");
-        int n = sc.nextInt();
-        String str ="";
-        str  = ((n%2==0) ? "0" : "1")+str;
-        int b = Integer.parseInt(str);
-        int pos = str.length();
-        int bitMask = 1<<pos;
-        int result = bitMask | b;
-        System.out.println(result);
-        // if(result%2==0){
-        //     System.out.println("Number is power of 2.");
-        // }else{
-        //     System.out.println("Number is not a power of 2.");
-        // }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number to check: ");
+            int n = sc.nextInt();
+            String str ="";
+            while(n>0){
+                str  = ((n%2==0) ? "0" : "1")+str;
+                n= n/2;
+            }
+            int b = Integer.parseInt(str);
+            int pos = str.length()-1;
+            int bitMask = 1<<pos;
+            int result = bitMask | b;
+            System.out.println(result);
+            if(result%2==0){
+                System.out.println("Number is power of 2.");
+            }else{
+                System.out.println("Number is not a power of 2.");
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
