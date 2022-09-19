6fbcfc5aaa4ce22918e71fd2551e7a9f75ed8586
ravinderchautala <ryvinder@gmail.com>
2022-09-20 00:12:51 +0530

    power problem space complexity reduced

--- apna_college/ch17_recursion/Power.java
+++ apna_college/ch17_recursion/Power.java
@@ -3,11 +3,24 @@ package apna_college.ch17_recursion;
 import java.util.Scanner;
 
 public class Power {
+    static int res;
     public static int pow(int n,int x){
         if(x==0){
             return 1;
         }
-        return n*pow(n,x-1);
+        if(n==0){
+            return 0;
+        }
+        // for space complexity = n;
+        // return n*pow(n,x-1);
+        
+        //  for space complexity = logn
+        if(x%2==0){
+            res = pow(n, x/2)*pow(n, x/2);
+        }else{
+            res = n*pow(n, x/2)*pow(n, x/2);
+        }
+        return res;
     }
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
