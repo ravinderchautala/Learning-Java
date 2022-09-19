7a06a8c7ff5a7e9ed2b89a3f33af3ecea555eecd
ravinderchautala <ryvinder@gmail.com>
2022-09-20 01:07:42 +0530

    reverse string problem solved

---
+++ apna_college/ch18_recursion_II/RevString.java
@@ -0,0 +1,16 @@
+package apna_college.ch18_recursion_II;
+
+public class RevString {
+    public static void revString(String str,int index){
+        if(index==0){
+            System.out.print(str.charAt(0));
+            return;
+        }
+        System.out.print(str.charAt(index));
+        revString(str, index-1);
+    }
+    public static void main(String[] args) {
+        String str = "Vinita";
+        revString(str, str.length()-1);
+    }
+}
