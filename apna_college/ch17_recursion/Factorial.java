56026f1c571fa8fb7bdcb1b4975f2bb1e0dbc1e4
ravinderchautala <ryvinder@gmail.com>
2022-09-19 23:11:30 +0530

    factorial problem  solved

---
+++ apna_college/ch17_recursion/Factorial.java
@@ -0,0 +1,16 @@
+package apna_college.ch17_recursion;
+
+public class Factorial {
+    public static int fac(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        int a = fac(n-1);
+         int fac = n*a;
+         return fac;
+    }
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(fac(n)); 
+    }
+}
