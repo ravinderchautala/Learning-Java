217c8c10ca07005c9f290270ea77971ff244c9d0
ravinderchautala <ryvinder@gmail.com>
2022-09-20 00:49:59 +0530

    tower of hanoi problem solved

---
+++ apna_college/ch18_recursion_II/TowerOfHanoi.java
@@ -0,0 +1,17 @@
+package apna_college.ch18_recursion_II;
+
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n,String source,String helper,String dest){
+        if(n==1){
+            System.out.println("Disk "+ n +" moved from "+ source + " to the rod "+ dest);
+            return;
+        }
+        towerOfHanoi(n-1,source, dest,helper);
+        System.out.println("Disk "+ n +" moved from "+ source + " to the rod "+ dest);
+        towerOfHanoi(n-1,helper, source, dest);
+    }
+    public static void main(String[] args) {
+        int n=2;
+        towerOfHanoi(n, "A", "B", "C");
+    }
+}
